7fa7986f46dda415bcafd307ddd67900a977e11d
joshua.browning@fao.org <joshua.browning@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2015-05-01 13:12:33 +0000

    Still trying to figure out how to use SVN... :)
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@2914 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- faoswsProduction.Rcheck/faoswsProduction/doc/faoswsProduction.R
+++
@@ -1,8 +0,0 @@
-## ----setup, include=FALSE, cache=FALSE----------------------------------------
-library(knitr)
-opts_chunk$set(fig.path='figure/', fig.align='center', fig.show='hold',
-               warning=FALSE, message=FALSE, error=FALSE, tidy=FALSE, 
-               results='markup', eval=TRUE, echo=TRUE, cache=FALSE, dpi=200)
-options(replace.assign=TRUE,width=80)
-assign("depthtrigger", 10, data.table:::.global)
-
