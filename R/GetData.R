371c0fd8734ea66a42c941f91c64d42d61551457
joshua.browning@fao.org <joshua.browning@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2015-05-19 14:20:54 +0000

    Added documentation to GetData about session Id
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@2942 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/GetData.R
+++ R/GetData.R
@@ -1,36 +1,45 @@
 ##' Get Data
 ##' 
-##' This function provides an interface between an R session and the database.
-##' Note that swsContext files must exist in your session, so you should run
+##' This function provides an interface between an R session and the database. 
+##' Note that swsContext files must exist in your session, so you should run 
 ##' GetTestEnvironment before calling this function.
+##' 
+##' NOTE: This function will either pull directly from the database or from the
+##' local session you set up.  This is important, as analysts may have changed
+##' values within a local session and may then execute GetData.  To avoid
+##' overwriting the values they updated, you should make sure to include the
+##' session ID in the key argument.  This is fairly easy to do: when
+##' constructing the DatasetKey, simply pass sessionId =
+##' slot(swsContext.datasets[[1]], "sessionId") as an argument.
 ##' 
 ##' If the denormalized value is not set then the data is normalized; if set the
 ##' data is denormalized along the axis specified.
 ##' 
-##' If the pivoting vector is present, the dimensions are extracted in the
-##' specified order and applying the requested sort direction. This affects
-##' both normalized and denormalized extractions. For normalized extractions
-##' only the order of the dimensions is influenced, while for denormalized
-##' extraction the effect is more evident since the last dimension specified in
-##' the pivoting vector gets its values developed along the column of the
-##' generated data.table result object.
+##' If the pivoting vector is present, the dimensions are extracted in the 
+##' specified order and applying the requested sort direction. This affects both
+##' normalized and denormalized extractions. For normalized extractions only the
+##' order of the dimensions is influenced, while for denormalized extraction the
+##' effect is more evident since the last dimension specified in the pivoting
+##' vector gets its values developed along the column of the generated
+##' data.table result object.
 ##' 
-##' @param key An object of class DatasetKey.  Often, this will be one of the
-##' list elements of swsContext.datasets (if running in a debug/local session,
-##' create this object with GetTestEnvironment).
-##' @param flags Logical, indicating if flags should be returned (TRUE)
-##' otherwise not returned (FALSE).
-##' @param normalized Logical, if true then data are returned in normalized
-##' format, otherwise the format is denormalized.
-##' @param pivoting A vector, each of whose elements must be an object of type
-##' Pivoting.  If omitted, no pivoting is performed on the dataset.  Using this
-##' argument can allow for convenient reshaping of the data prior to pulling it
-##' into R.  Note: if this argument is included, then all of the dimensions in
-##' key must be included in this vector.  See ?Pivoting for a description on
-##' creating this argument and for some examples on how to use it.
-##' 
+##' @param key An object of class DatasetKey.  Often, this will be one of the 
+##'   list elements of swsContext.datasets (if running in a debug/local session,
+##'   create this object with GetTestEnvironment).
+##' @param flags Logical, indicating if flags should be returned (TRUE) 
+##'   otherwise not returned (FALSE).
+##' @param normalized Logical, if true then data are returned in normalized 
+##'   format, otherwise the format is denormalized.
+##' @param pivoting A vector, each of whose elements must be an object of type 
+##'   Pivoting.  If omitted, no pivoting is performed on the dataset.  Using
+##'   this argument can allow for convenient reshaping of the data prior to
+##'   pulling it into R.  Note: if this argument is included, then all of the
+##'   dimensions in key must be included in this vector.  See ?Pivoting for a
+##'   description on creating this argument and for some examples on how to use
+##'   it.
+##'   
 ##' @return A data table containing the data matching the key (may be empty).
-##' 
+##'   
 ##' @examples
 ##' \dontrun{
 ##' # swsContext files are necessary for GetData to run (token may need to be updated)
