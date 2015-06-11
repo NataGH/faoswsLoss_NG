2d0406da65d3994947c721df95c3a71be1efa736
joshua.browning@fao.org <joshua.browning@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2015-06-11 06:13:54 +0000

    Added some documentation for DatasetKey() and Dimension()
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@2973 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/DatasetKey.R
+++ R/DatasetKey.R
@@ -1,18 +1,39 @@
 ##' Dataset Key Class
 ##' 
-##' @param domain A character value specifying the domain of interest, such as
-##' "agriculture".
-##' @param dataset A character value specifying the dataset within the domain,
-##' such as "agriculture".
-##' @param dimensions A list of objects, each of class Dimension.
-##' @param sessionId The ID of the session from which data should be accessed.
-##' If NULL, the database is used directly.
+##' This class provides a mechanism for pulling data from the SWS.  You must
+##' pass a DatasetKey object into the GetData function in order to read data
+##' from the SWS.
 ##' 
-##' @note Many of these variables (domain, dataset, and dimensions) can be
-##' found by examining some of the swsContext objects which are created in a
-##' GetTestEnvironment call (in a debug session).  Moreover, after executing
-##' GetTestEnvironment, the swsContext.datasets object will contain a list of
-##' objects of type DatasetKey.
+##' @param domain A character value specifying the domain of interest, such as 
+##'   "agriculture".
+##' @param dataset A character value specifying the dataset within the domain, 
+##'   such as "agriculture".
+##' @param dimensions A list of objects, each of class Dimension.
+##' @param sessionId The ID of the session from which data should be accessed. 
+##'   If NULL, the database is used directly.
+##'   
+##' @note Many of these variables (domain, dataset, and dimensions) can be found
+##'   by examining some of the swsContext objects which are created in a 
+##'   GetTestEnvironment call (in a debug session).  Moreover, after executing 
+##'   GetTestEnvironment, the swsContext.datasets object will contain a list of 
+##'   objects of type DatasetKey.  Additionally, information about these objects
+##'   can be found at the following url: 
+##'   http://hqlqasws1.hq.un.fao.org:8080/dataset_configuration.html
+##' 
+##' @examples
+##' \dontrun{
+##' dim1 = Dimension(name = "geographicAreaM49", keys = c("12", "24", "204"))
+##' dim2 = Dimension(name = "measuredElement", keys = c("51", "5510", "5607"))
+##' dim3 = Dimension(name = "measuredItemCPC", keys = c("0111", "0112", "0113"))
+##' dim4 = Dimension(name = "timePointYears", keys = as.character(1960:2010))
+##' 
+##' key = DatasetKey(domain = "agriculture", dataset = "agriculture",
+##'                  dimensions = list(dim1, dim2, dim3, dim4))
+##' 
+##' GetData(key = key)
+##' }
+##' 
+##' @seealso \code{\link{Dimension}}, \code{\link{GetData}}
 ##' 
 
 DatasetKey <- setClass("DatasetKey", 
--- R/Dimension.R
+++ R/Dimension.R
@@ -1,15 +1,39 @@
 ##' Dimension Class
 ##' 
-##' A dimension is represented by its name and by the vector of selected
-##' codes/keys.
+##' A dimension is represented by its name and by the vector of selected 
+##' codes/keys.  This object is then placed in a list with other Dimension 
+##' objects and passed to a DatasetKey object.
 ##' 
-##' @param name The name of the dimension to access, some examples are
-##' "timePointYears", "measuredElement", or "geographicAreaM49".  These
-##' correspond to the dimensions on the SWS, but the names are slightly
-##' different.  Currently, if you want to find the name of a dimension, you
-##' need to ask CIO.
-##' @param keys The key codes specifying what values should be pulled for the
-##' dimension.
+##' Note: for some reason, this function fails if the arguments are not named. 
+##' So, you will need to always specify "name =" and "key =" in your call to 
+##' Dimension.
+##' 
+##' @param name The name of the dimension to access, some examples are 
+##'   "timePointYears", "measuredElement", or "geographicAreaM49".  These 
+##'   correspond to the dimensions on the SWS, but the names are slightly 
+##'   different.  Currently, if you want to find the name of a dimension, you 
+##'   can check the following url: 
+##'   http://hqlqasws1.hq.un.fao.org:8080/dataset_configuration.html.
+##' @param keys The key codes specifying what values should be pulled for the 
+##'   dimension.
+##'   
+##' @return An object of class Dimension.  This is used to pass onto the 
+##'   DatasetKey class.
+##'   
+##' @examples
+##' \dontrun{
+##' dim1 = Dimension(name = "geographicAreaM49", keys = c("12", "24", "204"))
+##' dim2 = Dimension(name = "measuredElement", keys = c("51", "5510", "5607"))
+##' dim3 = Dimension(name = "measuredItemCPC", keys = c("0111", "0112", "0113"))
+##' dim4 = Dimension(name = "timePointYears", keys = as.character(1960:2010))
+##' 
+##' key = DatasetKey(domain = "agriculture", dataset = "agriculture",
+##'                  dimensions = list(dim1, dim2, dim3, dim4))
+##' 
+##' GetData(key = key)
+##' }
+##' 
+##' @seealso \code{\link{DatasetKey}}, \code{\link{GetData}}
 ##' 
 
 Dimension <- setClass("Dimension",
