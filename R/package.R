3d7ff60a9ccccb6a15276228ec6f16094500e657
Sebastian Campbell <sebastian.campbell@fao.org>
2015-12-17 16:12:14 +0100

    Documented vignette location

--- R/package.R
+++ R/package.R
@@ -2,16 +2,17 @@
 #' 
 #' This package allows access to the FAO SWS via its API when the appropriate 
 #' client certificates are present. The main workflow is to acquire a token from
-#' the SWS web interface and use the \code{\link{GetTestEnvironment}} function
-#' to open a session. After this, use the \code{\link{GetData}} function.
+#' the SWS web interface and use the \code{\link{GetTestEnvironment}} function 
+#' to open a session. After this, use the \code{\link{GetData}} function. To
+#' read the vignette: \code{vignette("R_API_2014")}
 #' 
 #' @docType package
 #' @name faosws-package
 #' @aliases faosws
 #' @author Engineering Ingegneria Informatica
-#' 
+#'   
 #' @include SetClientFiles.R
-#' 
+#'   
 #' @import data.table
 
 # prevent roxygen2 marking this as dataset
