bcde800efc7127b8096685802bdff0fb26d85841
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-01-12 12:32:10 +0000

    Added functional tests to run on server
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@4019 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- tests/testthat/test-authentication.R
+++ tests/testthat/test-authentication.R
@@ -2,21 +2,26 @@ library(faosws)
 
 context("Authentication")
 
-#Production URL and token
-prbaseUrl <- "https://hqlprswsas1.hq.un.fao.org:8181/sws"
-prtoken <- "d0caea7d-c1c8-4259-9e1f-7ecaf4343706" 
+#EA URL and token
+eabaseUrl <- "https://hqlqasws1.hq.un.fao.org:9453/sws"
+eatoken <- "50ced34b-2663-43c3-8691-eaad30924e16" 
 
+if(CheckDebug()){
+  certlocation <- "~/certificates/qa"
+  SetClientFiles(certlocation)
+}
 
-test_that("Authentication to production database", {
-  expect_is(object = GetTestEnvironment(prbaseUrl, prtoken)[[1]], "DatsetKey")
-  expect_error(GetTestEnvironment(prbaseUrl, "badtoken"))
-})
+## Skip if authentication fails
+#catch <- try(GetTestEnvironment(eabaseUrl, eatoken), silent = TRUE)
+#should_skip <- inherits(catch, "try-error")
 
+testAuthentication <- function(server="https://hqlprswsas1.hq.un.fao.org:8181/sws", 
+                               token="d0caea7d-c1c8-4259-9e1f-7ecaf4343706"){
+  #if(should_skip) skip("Unable to connect. Check the certificate locations")
+  GetTestEnvironment(server, token)[[1]]
+}
 
-#QA URL and token
-qabaseUrl <- "https://hqlqasws1.hq.un.fao.org:8181/sws" #qa
-qatoken <- "d858f232-9ecd-4af6-945d-b872cd140e9f" #qa
-
-test_that("Authentication should fail with the wrong certificates", {
-  expect_error(GetTestEnvironment(qabaseUrl, qatoken))
+test_that("Authentication to EA database", {
+  expect_is(object = testAuthentication(eabaseUrl, eatoken), "DatasetKey")
+  expect_error(testAuthentication(eabaseUrl, "badtoken"))
 })
