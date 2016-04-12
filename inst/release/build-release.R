a1d1ee960ed2960925bf68ca93ce9e1b55eb2148
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-04-12 12:42:44 +0000

    Fixed bug in build-release.sh where packages not in the repo were ignored
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@4516 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- inst/release/build-release.R
+++ inst/release/build-release.R
@@ -5,20 +5,20 @@ LIB <- arguments[1]
 setwd(arguments[2])
 
 # Get dependencies from description file
-deps <- paste0(read.dcf("DESCRIPTION", fields=c("Depends", "Imports", "Suggests"))[1,], collapse = "") 
+deps <- paste0(read.dcf("DESCRIPTION", fields=c("Depends", "Imports", "Suggests"))[1,], collapse = "")
 #Remove R dependency
 deps <- gsub("\nR |\\(.*?\\)", "", deps)
 deps <- strsplit(deps, ",?\n")[[1]]
 deps <- deps[deps != ""]
 
 # hardcode the fao-sws-cran repo for CRAN
-local({r = getOption("repos") 
-r["CRAN"] = "http://hqlprsws1.hq.un.fao.org/fao-sws-cran/"
-options(repos = r)
-})
+options(repos = c("CRAN" = "http://hqlprsws1.hq.un.fao.org/fao-sws-cran/"))
 
 # Get non-base packages
-packs <- intersect(deps, available.packages()[, "Package"])
+installed <- as.data.frame(installed.packages(lib.loc=.Library))
+
+packs <- setdiff(deps, 
+                   installed[installed[,"Priority" ] %in% c("base", "recommended") , "Package"])
 
 # Only install non-base packages
 to_install <- setdiff(packs, installed.packages(lib.loc=LIB)[,"Package"])
@@ -29,7 +29,7 @@ if(length(to_install) > 0){
   install.packages(to_install, lib = LIB)
   #Report results
   message(sprintf("Package(s) %s installed", paste(to_install, collapse = ", ")))
-  }
+}
 
 #Report results
 not_installed <- setdiff(packs, to_install)
