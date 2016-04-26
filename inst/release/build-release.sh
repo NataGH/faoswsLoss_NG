2586403651fed8f299f849d6b7f771fc254ef205
fabio.simeoni@fao.org <fabio.simeoni@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-04-26 13:34:46 +0000

    POM's version guides DESCRIPTION's
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@4672 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- inst/release/build-release.sh
+++ inst/release/build-release.sh
@@ -4,12 +4,19 @@ export ICUDT_DIR=$(readlink -m .)
 DIR=$(pwd)
 BASHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 FAOCRAN=http://hqlprsws1.hq.un.fao.org/fao-sws-cran/
+VERSION=$1
 
 # Make directory if it doesn't exist
 mkdir -p $R_LIBS
 
 # download deps, if required. then actually build
-Rscript "$BASHDIR/build-release.R" $R_LIBS $DIR $FAOCRAN && R CMD build .
+Rscript "$BASHDIR/build-release.R" $R_LIBS $DIR $FAOCRAN && 
+
+# rewrites version in DESCRIPTION
+sed -i -e "s/^Version: .*/Version: $VERSION/" DESCRIPTION  &&
+
+# triggers a standard R build
+R CMD build .
 
 if [ $? -gt 0 ]
 then 
