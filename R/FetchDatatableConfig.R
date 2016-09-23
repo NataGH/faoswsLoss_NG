f3ca6a161de257d6cf651c2f045d0d9b4b4348fb
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-09-23 16:27:37 +0000

    Added new function FetchDatatableNames
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@5255 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/FetchDatatableConfig.R
+++ R/FetchDatatableConfig.R
@@ -1,19 +1,26 @@
 #' Read Datatable metadata list
 #' 
-#' This function pulls metadata regarding Datatables. If 
-#' \code{\link{GetTestEnvironment}} is not called beforehand, a URL may be 
-#' supplied, allowing access to this metadata without a token. Of particular 
-#' note in each field is the 'label' element which gives the proper name of the 
-#' table as is displayed on the web interface of the Statistical Working System
-#'
+#' @aliases FetchDatatableNames
+#'   
+#' @description These functions pull metadata regarding Datatables. If 
+#'   \code{\link{GetTestEnvironment}} is not called beforehand, a URL may be 
+#'   supplied, allowing access to this metadata without a token. Of particular 
+#'   note in each field is the 'label' element which gives the proper name of
+#'   the table as is displayed on the web interface of the Statistical Working
+#'   System
+#'   
+#'   \code{FetchDatatableNames} is a simpler version of 
+#'   \code{FetchDatatableConfig} which gives the names of all available tables
+#'   
 #' @param tables character. Optional specification of tables of interest.
-#' @param baseurl character. If specified, uses this URL instead of looking for
-#'   one obtained from a token. Must be of the form
+#' @param baseurl character. If specified, uses this URL instead of looking for 
+#'   one obtained from a token. Must be of the form 
 #'   \code{http(s)://hostname:port/sws/rest}.
-#'
-#' @return A list containing table metadata
-#'
-#' @export FetchDatatableConfig
+#'   
+#' @return For \code{FetchDatatableConfig}, a list containing Datatable metadata.
+#'   For \code{FetchDatatableNames}, a character vector of available Datatables.
+#'   
+#' @export FetchDatatableConfig FetchDatatableNames
 
 FetchDatatableConfig <- function(tables, baseurl = swsContext.baseRestUrl){
   rawDt <- GetRestCall(paste0(baseurl, "/datatable"))
@@ -34,6 +41,11 @@ FetchDatatableConfig <- function(tables, baseurl = swsContext.baseRestUrl){
   Dt
 }
 
+#' @rdname FetchDatatableConfig
+FetchDatatableNames <- function(baseurl = swsContext.baseRestUrl){
+  rawDt <- GetRestCall(paste0(baseurl, "/datatable"))
+  vapply(rawDt, getID, character(1))
+}
 
 
 getID <- function(cols){
