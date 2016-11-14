85c6825ab202f0f82f6fcd0913d5a4d02a1eb703
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-11-14 10:34:25 +0000

    Modified ReadQualityIndicators to look in lists correctly
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@5448 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/ReadQualityIndicators.R
+++ R/ReadQualityIndicators.R
@@ -7,39 +7,42 @@
 #' @export ReadQualityIndicators
 
 ReadQualityIndicators <- function(dataset){
-
+  
   stopifnot(is.character(dataset), length(dataset) == 1L)
-
+  
   url <- paste0(swsContext.baseRestUrl, "/api/metadata/instance/dataset/", dataset, "/latest?_xid=", swsContext.token)
   quality_indicators <- GetRestCall(url, nullValue = NA)
-  quality_indicators[["content"]] <- as.list(quality_indicators[["content"]])
-
-  # Find objects that should be tables
-  quality_indicators[["content"]] <- FormatIndicatorList(quality_indicators[["content"]])
-
+  
+  quality_indicators <- lapply(quality_indicators, function(x){
+    # Find objects that should be tables
+    x[["content"]] <- as.list(x[["content"]])
+    x[["content"]]  <- FormatIndicatorList(x[["content"]])
+    x
+  })
+  
   quality_indicators
-
+  
 }
 
 FormatIndicatorList <- function(indicatorList){
-
+  
   new_indicatorList <- indicatorList
-
+  
   if(is.atomic(new_indicatorList)) return(new_indicatorList)
   
   if(any(vapply(new_indicatorList, Negate(is.atomic), logical(1)))) {
     return(lapply(new_indicatorList, FormatIndicatorList))
-    }
+  }
   
   can_data.table <- function(x){all(all(vapply(x, is.atomic, logical(1))) &
                                       length(x) > 1 &
                                       all(diff(lengths(x)) == 0L))}
-
+  
   #table_candidate_names <- names(Filter(can_data.table, new_indicatorList[["content"]]))
   if(can_data.table(new_indicatorList)){
     new_indicatorList <- as.data.table(new_indicatorList, stringsAsFactors = FALSE)
   }
-
+  
   new_indicatorList
-
+  
 }
