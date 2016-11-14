30083694d8a8fd46eadf6bf9016c20c498017477
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-11-14 10:34:22 +0000

    Added ability to delete values.
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@5443 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/WriteQualityIndicators.R
+++ R/WriteQualityIndicators.R
@@ -8,6 +8,8 @@
 #'   contain Quality Indicators that you wish to add or modify. Additions and
 #'   modifications occur at the top level of the list.
 #'   
+#'   In order to delete a value, simply have a top-level list element named as NULL.
+#'   
 #' @export WriteQualityIndicators
 
 WriteQualityIndicators <- function(indicators, dataset){
@@ -16,6 +18,9 @@ WriteQualityIndicators <- function(indicators, dataset){
   
   # If there is no existing metadata, create it
   if(length(prev) == 1L && prev == ""){
+    
+    # Remove any NULL values
+    indicators <- indicators[!vapply(indicators, is.null, logical(1))]
     
     json <- list(
       model = "sws-indicators",
@@ -32,7 +37,11 @@ WriteQualityIndicators <- function(indicators, dataset){
     
   } else {
     
-    # Find elements to replace and add
+    # Find elements to replace, add and remove
+    ## Find names to be deleted then remove them from indicators to be written
+    null_names <- names(indicators[vapply(indicators, is.null, logical(1))])
+    indicators[null_names] <- NULL
+    ## Find names to be replaced and brand new ones to be added
     replacement_names <- intersect(names(indicators), names(prev[["content"]]))
     new_names <- setdiff(names(indicators), replacement_names)
     
@@ -41,6 +50,10 @@ WriteQualityIndicators <- function(indicators, dataset){
     
     if(length(replacement_names) > 0){
     new_indicators[replacement_names] <- NULL
+    }
+    
+    if(length(null_names) > 0){
+      new_indicators[null_names] <- NULL
     }
     
     new_indicators <- c(new_indicators, indicators[c(new_names, replacement_names)])
