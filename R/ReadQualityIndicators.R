67830ec19932c9558cd3c784170af7e6ac8571fc
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-11-14 10:34:24 +0000

    Fixed indicator formatting function
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@5446 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/ReadQualityIndicators.R
+++ R/ReadQualityIndicators.R
@@ -1,40 +1,45 @@
 #' Read Quality Indicators from dataset
-#' 
+#'
 #' These functions allow reading and writing of quality indicators, Quality indicators are supplied as lists.
-#' 
+#'
 #' @param dataset character. Name of an SWS dataset
-#' 
+#'
 #' @export ReadQualityIndicators
 
 ReadQualityIndicators <- function(dataset){
-  
+
   stopifnot(is.character(dataset), length(dataset) == 1L)
-  
+
   url <- paste0(swsContext.baseRestUrl, "/api/metadata/instance/dataset/", dataset, "/latest?_xid=", swsContext.token)
-  quality_indicators <- GetRestCall(url)
+  quality_indicators <- GetRestCall(url, nullValue = NA)
   quality_indicators[["content"]] <- as.list(quality_indicators[["content"]])
-  
+
   # Find objects that should be tables
-  quality_indicators <- FormatIndicatorList(quality_indicators)
+  quality_indicators[["content"]] <- FormatIndicatorList(quality_indicators[["content"]])
 
   quality_indicators
-  
+
 }
 
 FormatIndicatorList <- function(indicatorList){
-  
+
   new_indicatorList <- indicatorList
+
+  if(is.atomic(new_indicatorList)) return(new_indicatorList)
   
-  table_candidate_names <- names(Filter(function(x){all(all(vapply(x, is.atomic, logical(1))) & 
-                                                          length(x) > 1 & 
-                                                          all(diff(lengths(x)) == 0L))}, new_indicatorList[["content"]]))
-  
-  if(length(table_candidate_names) > 0){
-    for(i in table_candidate_names){
-      new_indicatorList[["content"]][[i]] <- as.data.table(new_indicatorList[["content"]][[i]])
+  if(any(vapply(new_indicatorList, Negate(is.atomic), logical(1)))) {
+    return(lapply(new_indicatorList, FormatIndicatorList))
     }
+  
+  can_data.table <- function(x){all(all(vapply(x, is.atomic, logical(1))) &
+                                      length(x) > 1 &
+                                      all(diff(lengths(x)) == 0L))}
+
+  #table_candidate_names <- names(Filter(can_data.table, new_indicatorList[["content"]]))
+  if(can_data.table(new_indicatorList)){
+    new_indicatorList <- as.data.table(new_indicatorList, stringsAsFactors = FALSE)
   }
-  
+
   new_indicatorList
-  
+
 }
--- R/WriteQualityIndicators.R
+++ R/WriteQualityIndicators.R
@@ -1,33 +1,33 @@
 #' Write Quality Indicators to dataset
-#' 
+#'
 #' @param indicators list. List of indicators to modify or add.
 #' @param dataset character. Name of dataset to which to write Quality
 #'   indicators
-#'   
+#'
 #' @details The dataset may be supplied as a nested list. The list should
 #'   contain Quality Indicators that you wish to add or modify. Additions and
 #'   modifications occur at the top level of the list.
-#'   
+#'
 #'   In order to delete a value, simply have a top-level list element named as NULL.
-#'   
+#'
 #' @export WriteQualityIndicators
 
 WriteQualityIndicators <- function(indicators, dataset){
-  
+
   stopifnot(is.character(dataset), length(dataset) == 1L, is.list(indicators))
-  
+
   if(length(names(indicators)) != length(indicators)){
     stop("All elements of indicators must be named")
   }
-  
+
   prev <- ReadQualityIndicators(dataset)
-  
+
   # If there is no existing metadata, create it
   if(length(prev) == 1L && prev == ""){
-    
+
     # Remove any NULL values
     indicators <- indicators[!vapply(indicators, is.null, logical(1))]
-    
+
     json <- list(
       model = "sws-indicators",
       version = "0.1",
@@ -37,12 +37,12 @@ WriteQualityIndicators <- function(indicators, dataset){
       ),
       content = indicators
     )
-    
+
     newurl <- paste0(swsContext.baseRestUrl, "/api/metadata/instance/all?_xid=", swsContext.token)
-    response <- PostRestCall(newurl, json)
-    
+    response <- PostRestCall(newurl, json, nullValue = NA)
+
   } else {
-    
+
     # Find elements to replace, add and remove
     ## Find names to be deleted then remove them from indicators to be written
     null_names <- names(indicators[vapply(indicators, is.null, logical(1))])
@@ -50,20 +50,20 @@ WriteQualityIndicators <- function(indicators, dataset){
     ## Find names to be replaced and brand new ones to be added
     replacement_names <- intersect(names(indicators), names(prev[["content"]]))
     new_names <- setdiff(names(indicators), replacement_names)
-    
+
     # Remove indicators present in both and replace them with new indicators
     new_indicators <- prev[["content"]]
-    
+
     if(length(replacement_names) > 0){
     new_indicators[replacement_names] <- NULL
     }
-    
+
     if(length(null_names) > 0){
       new_indicators[null_names] <- NULL
     }
-    
+
     new_indicators <- c(new_indicators, indicators[c(new_names, replacement_names)])
-    
+
     json <- c(list(
       id = prev[["id"]],
       model = "sws-indicators",
@@ -76,14 +76,14 @@ WriteQualityIndicators <- function(indicators, dataset){
     ),
     prev[c("createdOn", "lastModifiedOn", "createdBy", "lastModifiedBy")]
     )
-    
+
     # Send newly compiled indicators to replace the old ones
     newurl <- paste0(swsContext.baseRestUrl, "/api/metadata/instance/all/", prev[["id"]], "?_xid=", swsContext.token)
-    response <- PutRestCall(newurl, json)
+    response <- PutRestCall(newurl, json, nullValue = NA)
   }
-  
+
   # Return modified indicators as returned by the server
-  response <- FormatIndicatorList(response)
+  response[["content"]] <- FormatIndicatorList(response[["content"]])
   as.list(response[["content"]])
-  
+
 }
