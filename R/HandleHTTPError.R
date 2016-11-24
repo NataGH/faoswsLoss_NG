9fc3905dfa603a038a21fb0fb33448ad7c05e3c5
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-11-24 13:46:02 +0000

    HandleHTTPError now deals with missing status and response
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@5507 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- R/HandleHTTPError.R
+++ R/HandleHTTPError.R
@@ -11,11 +11,18 @@
 #' 
 
 HandleHTTPError <- function(status, response){
-    erresponse <- try(fromJSON(response), silent = TRUE)
-    if(inherits(erresponse, "try-error")) stop("SWS could not return reason for error. HTTP error " , status, "\n", erresponse)
-    message <- ifelse(exists("message", erresponse), paste0("\nError message: ", erresponse[["message"]]), "")
-    details <- ifelse(exists("details", erresponse), paste0("\nDetails: ", erresponse[["details"]]), "")
-    
-    stop(paste("Unable to perform REST call to SWS server. Status code was", status, message, details))
+  
+  if (missing(status)){
+    status <- "unavailable"
+  }
+  if(missing(response)){
+    stop(paste0("SWS could not return reason for error. HTTP status: ", status))
+  } 
+  erresponse <- try(RJSONIO::fromJSON(response), silent = TRUE)
+  if(inherits(erresponse, "try-error")) stop("SWS could not return reason for error. HTTP error " , status, "\n", erresponse)
+  message <- ifelse(exists("message", erresponse), paste0("\nError message: ", erresponse[["message"]]), "")
+  details <- ifelse(exists("details", erresponse), paste0("\nDetails: ", erresponse[["details"]]), "")
+  
+  stop(paste("Unable to perform REST call to SWS server. Status code was", status, message, details))
   
 }
