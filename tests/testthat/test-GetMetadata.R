1509b9a642442caa23804b5a0d5a36426fba5013
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2016-11-24 13:45:58 +0000

    Added new test for GetMetadata (SWS-1556) and normalizeData
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@5501 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

---
+++ tests/testthat/test-GetMetadata.R
@@ -0,0 +1,32 @@
+context("GetMetadata")
+
+test_that("GetMetadata produces data in the right format",{
+  # Test introduced due to SWS-1556
+  testdata <- structure(list(keyDefinitions = list(structure(c("geographicAreaM49", 
+  "Geographic Area", "normal"), .Names = c("code", "description", 
+  "type")), structure(c("measuredElement", "Element", "measurementUnit"
+  ), .Names = c("code", "description", "type")), structure(c("measuredItemCPC", 
+  "Item", "normal"), .Names = c("code", "description", "type")), 
+  structure(c("timePointYears", "Year", "time"), .Names = c("code", 
+  "description", "type"))), data = list(list("4", "5417", "02111", 
+  "2005", 0, list(list("COMPUTATION_INFO", "Computation Information", 
+  "en", list(c("PARAMETER", "Parameter", "updateAll = session"
+  ), c("COMPUTATION", "Computation", "Compute Yield"), 
+  c("PARAMETER", "Parameter", "Main dataset Agriculture Production/Agriculture Production on session APR 2015-12-08 11:54:20 (524168)"
+  ))), list("GENERAL", "General", "en", list(c("COMMENT", 
+  "Comment", "The observation was rolled back to the version 1"
+  ))))))), .Names = c("keyDefinitions", "data"))
+ 
+  expected_output <- structure(list(geographicAreaM49 = c("4", "4", "4", "4"), measuredElement = c("5417", 
+    "5417", "5417", "5417"), measuredItemCPC = c("02111", "02111", 
+    "02111", "02111"), timePointYears = c("2005", "2005", "2005", 
+    "2005"), Metadata = c("PARAMETER", "COMPUTATION", "PARAMETER", 
+    "COMMENT"), Metadata_Language = c("en", "en", "en", "en"), Metadata_Group = c(1L, 
+    2L, 3L, 1L), Metadata_Value = c("updateAll = session", "Compute Yield", 
+                                                                                                                                                                            "Main dataset Agriculture Production/Agriculture Production on session APR 2015-12-08 11:54:20 (524168)", 
+                                                                                                                                                                            "The observation was rolled back to the version 1")), .Names = c("geographicAreaM49", 
+                                                                                                                                                                                                                                             "measuredElement", "measuredItemCPC", "timePointYears", "Metadata", 
+                                                                                                                                                                                                                                             "Metadata_Language", "Metadata_Group", "Metadata_Value"), row.names = c(NA, 
+                                                                                                                                                                                                                                                                                                                     -4L), class = c("data.table", "data.frame"))
+  expect_equal(GetData.NEW_processNormalizedResultMetadata(testdata), expected_output)
+})
--- tests/testthat/test-SaveData.R
+++ tests/testthat/test-SaveData.R
@@ -14,3 +14,27 @@ test_that("SaveData validates correctly",{
     SaveData.validate("agriculture", "aproduction", testDenormalisedData, normalized = FALSE)
     ))
 })
+
+test_that("normalizeData works correctly", {
+
+  testdata_normalizeData <- structure(list(measuredItemCPC = c("Beef", "Wheat"), Value_area_France = c(803.493349812925, 
+    847.820020280778), Value_area_Spain = c(NA, 246.175302891061)), .Names = c("measuredItemCPC", 
+    "Value_area_France", "Value_area_Spain"), row.names = c(NA, -2L
+    ), class = c("data.table", "data.frame"))
+  
+  expected_value <- structure(list(measuredItemCPC = c("Beef", "Wheat", "Wheat"), 
+    area = c("France", "France", "Spain"), Value = c(803.493349812925, 
+    847.820020280778, 246.175302891061)), .Names = c("measuredItemCPC", 
+    "area", "Value"), row.names = c(NA, -3L), class = c("data.table", 
+    "data.frame"))
+  expected_value_NA <- structure(list(measuredItemCPC = c("Beef", "Beef", "Wheat", "Wheat"
+    ), area = c("France", "Spain", "France", "Spain"), Value = c(803.493349812925, 
+    NA, 847.820020280778, 246.175302891061)), .Names = c("measuredItemCPC", 
+    "area", "Value"), row.names = c(NA, -4L), class = c("data.table", 
+    "data.frame"))
+  
+  expect_equal(normalizeData(testdata_normalizeData, "measuredItemCPC", "area", keepNA = FALSE),
+               expected_value)
+  expect_equal(normalizeData(testdata_normalizeData, "measuredItemCPC", "area"),
+               expected_value_NA)
+})
