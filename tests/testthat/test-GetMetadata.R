5d3aff3c211e4c5aae8d8b0b5c8e67b70b31890e
sebastian.campbell@fao.org <sebastian.campbell@fao.org@c4045de4-bbbe-11e2-9413-f56c8d7c6d4f>
2017-04-05 09:37:31 +0000

    The outputs of GetHistory and GetMetadata have changed and so their tests have to change.
    git-svn-id: https://svn.fao.org/projects/SWS/prj/R/trunk/faosws@6398 c4045de4-bbbe-11e2-9413-f56c8d7c6d4f

--- tests/testthat/test-GetMetadata.R
+++ tests/testthat/test-GetMetadata.R
@@ -18,15 +18,17 @@ test_that("GetMetadata produces data in the right format",{
   ))))))), .Names = c("keyDefinitions", "data"))
  
   expected_output <- structure(list(geographicAreaM49 = c("4", "4", "4", "4"), measuredElement = c("5417", 
-    "5417", "5417", "5417"), measuredItemCPC = c("02111", "02111", 
-    "02111", "02111"), timePointYears = c("2005", "2005", "2005", 
-    "2005"), Metadata = c("PARAMETER", "COMPUTATION", "PARAMETER", 
-    "COMMENT"), Metadata_Language = c("en", "en", "en", "en"), Metadata_Group = c(1L, 
-    2L, 3L, 1L), Metadata_Value = c("updateAll = session", "Compute Yield", 
-                                                                                                                                                                            "Main dataset Agriculture Production/Agriculture Production on session APR 2015-12-08 11:54:20 (524168)", 
-                                                                                                                                                                            "The observation was rolled back to the version 1")), .Names = c("geographicAreaM49", 
-                                                                                                                                                                                                                                             "measuredElement", "measuredItemCPC", "timePointYears", "Metadata", 
-                                                                                                                                                                                                                                             "Metadata_Language", "Metadata_Group", "Metadata_Value"), row.names = c(NA, 
-                                                                                                                                                                                                                                                                                                                     -4L), class = c("data.table", "data.frame"))
+                      "5417", "5417", "5417"), measuredItemCPC = c("02111", "02111", 
+                      "02111", "02111"), timePointYears = c("2005", "2005", "2005", 
+                      "2005"), Metadata = c("COMPUTATION_INFO", "COMPUTATION_INFO", 
+                      "COMPUTATION_INFO", "GENERAL"), Metadata_Element = c("PARAMETER", 
+                      "COMPUTATION", "PARAMETER", "COMMENT"), Metadata_Language = c("en", 
+                      "en", "en", "en"), Metadata_Group = c(1L, 2L, 3L, 1L), Metadata_Value = c("updateAll = session", 
+                      "Compute Yield", "Main dataset Agriculture Production/Agriculture Production on session APR 2015-12-08 11:54:20 (524168)", 
+                      "The observation was rolled back to the version 1")), .Names = c("geographicAreaM49", 
+                      "measuredElement", "measuredItemCPC", "timePointYears", "Metadata", 
+                      "Metadata_Element", "Metadata_Language", "Metadata_Group", "Metadata_Value"
+                      ), row.names = c(NA, -4L), class = c("data.table", "data.frame"))
+  
   expect_equal(GetData.processNormalizedResultMetadata(testdata), expected_output)
 })
