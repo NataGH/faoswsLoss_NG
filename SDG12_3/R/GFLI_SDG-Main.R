903727eee29905e110778cd2d4d0835c24d92e4d
AEENRA <aliciaenglish@enra-rc.org>
2018-02-05 17:21:05 +0100

    Capital letter changes for the SWS

--- SDG12_3/R/GFLI_SDG-Main.R
+++ SDG12_3/R/GFLI_SDG-Main.R
@@ -42,51 +42,33 @@ suppressMessages({
 })
 
 
-##################### For deletion #####################################
-## For Local 
-## SWS Connection
 
-githubsite <- '~/faoswsLoss/data-raw/'
-dirmain <-  '~/faoswsLoss'
-SetClientFiles(dir = "C:\\Users\\ENGLISHA\\Documents\\certificates\\qa")
-files = dir("~/Github/faoswsLoss/R",
-            full.names = TRUE) 
+if(CheckDebug()){
+  message("Not on server, so setting up environment...")
+  USER <- if_else(.Platform$OS.type == "unix",
+                  Sys.getenv('USER'),
+                  Sys.getenv('USERNAME'))
 
 
-token3 = "ec71e788-c7fa-44bc-a732-278d84f071fd"
+  library(faoswsModules)
+  settings <- ReadSettings(file = file.path(paste(dirmain,"sws.yml", sep='/')))
+  #SetClientFiles(settings[["certdir"]])
 
+  GetTestEnvironment(
+    baseUrl = settings[["server"]],
+    token = settings[["token"]]
+  )
 
-GetTestEnvironment(
-  baseUrl = "https://hqlqasws1.hq.un.fao.org:8181/sws",
-  token = token3
-) 
-
-# if(CheckDebug()){
-#   message("Not on server, so setting up environment...")
-#   USER <- if_else(.Platform$OS.type == "unix",
-#                   Sys.getenv('USER'),
-#                   Sys.getenv('USERNAME'))
-#   
-#   
-#   library(faoswsModules)
-#   settings <- ReadSettings(file = file.path(paste(dirmain,"sws.yml", sep='/')))
-#   #SetClientFiles(settings[["certdir"]])
-#   
-#   GetTestEnvironment(
-#     baseUrl = settings[["server"]],
-#     token = settings[["token"]]
-#   )
-#   
 }
 
 ######### Options ############
 selectedYear = as.character(1991:2015)
 ComparisonYear = as.character(c(2013,2015))
 
-GFLI_calc <- 1
+gfli_calc <- TRUE
 aggregation <- "geographicAreaM49" #"sdg_region", "geographicAreaM49"
 weights <- "intl_prices" 
-basketN <- "top2perhead_byCtry" # "top2perhead_Globatop10","top2_calories"
+basketn <- "top2perhead_byCtry" # "top2perhead_Globatop10","top2_calories"
 #####################
 
 areaVar = "geographicAreaM49"
@@ -97,8 +79,8 @@ keys =c(areaVar,yearVar,itemVar)
 keys_lower =tolower(keys)
 
 
-if(GFLI_calc){
-  GlobalfoodLoss <- GFLI_SDG_fun(selectedYear,keys_lower,aggregation,weights,basketN,production)
+if(gfli_calc){
+  GlobalfoodLoss <- GFLI_SDG_fun(selectedYear,keys_lower,aggregation,weights,basketn,production)
   
   ### Table for   
   FLI_y1 <- foodLossIndex %>% filter(timepointyears == min(ComparisonYear))
@@ -112,8 +94,8 @@ if(GFLI_calc){
   
 }  
 
-date = "_18Dec17"
-write.table(delta13_15,paste(githubsite, 'General/delta13_16',date, '.csv', sep=''),sep=',' )
-write.table(foodLossIndex,paste(githubsite, 'General/foodLossIndex',date, '.csv', sep=''),sep=',' )
-write.table(   basket ,paste(githubsite, 'General/Commodbasket',date, '.csv', sep=''),sep=',' )
-write.table(GFLI_Weight,paste(githubsite, 'General/weights',date, '.csv', sep=''),sep=',' )
+# date = "_18Dec17"
+# write.table(delta13_15,paste(githubsite, 'General/delta13_16',date, '.csv', sep=''),sep=',' )
+# write.table(foodLossIndex,paste(githubsite, 'General/foodLossIndex',date, '.csv', sep=''),sep=',' )
+# write.table(   basket ,paste(githubsite, 'General/Commodbasket',date, '.csv', sep=''),sep=',' )
+# write.table(GFLI_Weight,paste(githubsite, 'General/weights',date, '.csv', sep=''),sep=',' )
